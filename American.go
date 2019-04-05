5e8b91d331699d962eefaa8e5bd68c02386a75c7
bimalkeeth@gmail.com <Scala@1234>
2019-04-06 09:43:45 +1100

    channels

--- American.go
+++ American.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 func main() {
@@ -32,9 +35,44 @@ func main() {
 
 }
 
-func americanise(file *os.File, file2 *os.File) error {
+var britishAmerican = "british-american.txt"
+
+func americanise(inFile io.Reader, outFile io.Writer) (err error) {
+
+	reader := bufio.NewReader(inFile)
+	writer := bufio.NewWriter(outFile)
+
+	defer func() {
+		if err == nil {
+			err = writer.Flush()
+		}
+	}()
+	var replacer func(string) string
+	if replacer, err = makeReplaceFunction(britishAmerican); err != nil {
+		return err
+	}
+	wordRx := regexp.MustCompile("[A-Za-z]+")
+	eof := false
+	for !eof {
+		var line string
+		line, err = reader.ReadString('\n')
+		if err == io.EOF {
+			err = nil
+			eof = true
+		} else {
+			return err
+		}
+		line = wordRx.ReplaceAllStringFunc(line, replacer)
+		if _, err = writer.WriteString(line); err != nil {
+			return err
+		}
+	}
 
 	return nil
+}
+
+func makeReplaceFunction(s string) (func(string) string, error) {
+
 }
 
 func filesNamesFromCommandline() (inFileName string, outFileName string, err error) {
